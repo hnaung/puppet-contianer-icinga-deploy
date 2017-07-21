fe029fc68798be2043b0d613862dda907febb325
hnaung <unixaung@gmail.com>
2017-07-22 03:06:47 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ docker run -p 3900:3306 --name mysql -e MYSQL_ROOT_PASSWORD=ubuntu -d mysql:late
 
 git clone git@github.com:hnaung/puppet-contianer-icinga-deploy.git && cd puppet-contianer-icinga-deploy && docker build -t puppet-contianer-icinga-deploy/local:apache .
 
-docker run -d -p 80:80 --name apache --link mysql:mysql -v /var/www/html:/var/www/html test-deploy/local:apache
+docker run -d -p 80:80 --name apache --link mysql:mysql -v /var/www/html:/var/www/html puppet-contianer-icinga-deploy/local:apache
 
 cp db.php index.php logout.php /var/www/html/
 cp cronlogs.sh /root && chmod +x /root/cronlogs.sh
